a999a4ed60640269b6ee541c0c3557db3bd564a0
MKFMIKU <kfm@clfstudio.com>
2017-12-23 14:52:19 +0800

    debug

--- build.cmd
+++ build.cmd
@@ -1,4 +1,4 @@
 DEL build
 DEL dist
 
-pyinstaller main.py -p C:\miniconda35\envs\test-environment\lib\site-packages\PyQt5\Qt\bin -w
+pyinstaller main.py -p C:\miniconda35\envs\test-environment\lib\site-packages\PyQt5\Qt\bin
